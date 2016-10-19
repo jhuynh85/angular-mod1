1c86de9770f2d2161ddafd4a222ce015ba412158
Joseph Huynh <joe.huynh@gmail.com>
2016-10-18 23:20:09 -0700

    Assignment 1 completed

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 (function(){
+  'use strict';
+
   angular.module("LunchCheck", [])
 
   .controller('LunchCheckController', init);
 
   init.$inject = ['$scope'];
   function init($scope){
+
     $scope.checkIfToomuch = function () {
-
-    }
+      var items = $scope.string.split(',');
+      if (items.length==1 && items[0]==""){
+        $scope.message = "Please enter data first";
+      } else if (items.length <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    };
   }
-
-
 })();
