b863d6f15c1f49779d9db193bf329a7ca89611c6
Joseph Huynh <joe.huynh@gmail.com>
2016-10-18 22:37:34 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+(function(){
+  angular.module("LunchCheck", [])
+
+  .controller('LunchCheckController', init);
+
+  init.$inject = ['$scope'];
+  function init($scope){
+    $scope.checkIfToomuch = function () {
+
+    }
+  }
+
+
+})();
